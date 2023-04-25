952d7c39c421d46eba68022710e897089a74c493
Manon-Arc <arcasmanon47@gmail.com>
2023-04-25 12:22:00 +0200

    add pickcolor and comb

--- colors.go
+++ colors.go
@@ -1,14 +1,21 @@
-package main
+package MasterMind
 
 import (
-
-"math/rand"
-"time"
+	"math/rand"
+	"time"
 )
 
 var colors = []string{"bleu", "vert", "orange", "rouge", "blanc", "rose", "gris"}
 
-func pickColor() string {
-    rand.Seed(time.Now().UnixNano())
-    return colors[rand.Intn(len(colors))]
+func pickColor() string { // pick a random color in the list "colors"
+	rand.Seed(time.Now().UnixNano())
+	return colors[rand.Intn(len(colors))]
+}
+
+func comb() []string {
+	tab := []string{}
+	for i := 0; i < 4; i++ {
+		tab = append(tab,pickColor())
+	}
+	return tab
 }
---
+++ main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	color "/colors.go"
+)
+
+func main() {
+	color.comb()
+}
